c8e4f15d9dff02433efc37ef043aee76b3745434
Tatjana Kuznetsova <tanjak@tlu.ee>
2018-01-14 21:38:46 +0200

    lisasin kommentaaride saamine iga postitusele

--- comment.php
+++ comment.php
@@ -13,6 +13,65 @@
 	
 	//Vottab yhe ID ja naitab temast infot
 	$p = getsingleId($_GET["id"]);
+	
+
+	//kommentaaride saamine
+	function send_comment($comment){
+	
+		$mysqli = new mysqli($GLOBALS["serverHost"], 
+		$GLOBALS["serverUsername"], 
+		$GLOBALS["serverPassword"], 
+		$GLOBALS["database"]);
+		
+		$comment = $_POST["comment"];
+		
+		$stmt = $mysqli ->prepare("INSERT INTO pr_comments ( id_post, comment, email) VALUE(?, ?, ?)");
+		echo $mysqli->error;
+		$stmt->bind_param("iss", $_GET["id"], $comment, $_SESSION["userEmail"]);
+	
+		if($stmt->execute() ) {			
+		}
+	}
+	
+	//Kommentaaride naitamine
+	function show_comment(){
+		
+		$mysqli = new mysqli($GLOBALS["serverHost"], 
+		$GLOBALS["serverUsername"], 
+		$GLOBALS["serverPassword"], 
+		$GLOBALS["database"]);
+		
+		$stmt = $mysqli->prepare("
+		SELECT comment, email
+		FROM pr_comments
+		WHERE id_post = ?
+		");
+		
+		$stmt->bind_param("s", $_GET["id"]);
+		$stmt->bind_result($comment, $email );
+		$stmt->execute();
+		$results = array();
+		
+		while ($stmt->fetch()) {
+			$human = new StdClass();
+			$human->comment = $comment;
+			$human->email = $email;
+			array_push($results, $human);	
+		}
+		return $results;
+	}
+	
+	//kommentaaride salvestaime
+	if (isset ($_POST["comment"]) &&
+		!empty ($_POST["comment"])
+		)
+		
+	//kommentaaride saamine
+	{
+	send_comment($_GET["id"],$_POST["comment"],$_SESSION["userEmail"]);
+	}
+	
+	$people = show_comment();
 ?>
 
 
@@ -39,5 +98,27 @@ $html = "<table>";
 $html .= "</table>";
 echo $html
 ?>
+
+
+	<form method="POST">
+
+	<input name="comment" class="text" placeholder="Jata kommentaar">	
+	<br><input type="submit" value="Saada"></br>
+
+	</form>
+
+	
+<?php 
+$html1 = "<table>";
+	foreach ($people as $p) {
+	$html1 .= "<tr>";
+		$html1 .= "<td>".$p->email."</td>";
+		$html1 .= "<td>".$p->comment."</a></td>";
+	$html1 .= "</tr>";
+	}
+$html1 .= "</table>";
+echo $html1
+?>
+	
 
 </html>
